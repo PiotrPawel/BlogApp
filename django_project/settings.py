b4f3d6eb8f0c9c10c72d5a534b9675851eb6bc32
Piotr <pitero1989@o2.pl>
2019-10-10 18:12:59 +0200

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')3ad&n1fhi26dg00wvip5!d9bl4vs1c7f&@_o3-vmwr+v4%94u'
+#  SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = ')3ad&n1fhi26dg00wvip5!d9bl4vs1c7f&@_o3-vmwr+v4%94u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myblogindjangoapp.herokuapp.com']
 
 
 # Application definition
